655ef00f7e665009f024baadb52bf2c6a673b0e0
declanosullivan <declanseer@gmail.com>
2022-05-16 13:43:36 +0000

    Add varibale to replace secret key

--- marine_breeze/marine_breeze/settings.py
+++ marine_breeze/marine_breeze/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k1qals$mzxkqcgxk$tned=1xa7b2x34vqk97de*lm!oq!ugf73'
+if os.path.exists("env.py"):
+    import env  # noqa
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
