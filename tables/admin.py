31ecfa904a7f5ee78405a134ded95d8346949d6d
declanosullivan <declanseer@gmail.com>
2022-05-19 01:01:42 +0000

    Add models for bookings restaurant and tables

--- tables/admin.py
+++ tables/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Bookings
+from .models import Tables
 
 # Register your models here.
-admin.site.register(Bookings)
+admin.site.register(Tables)
